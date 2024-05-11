863da86b89abcbd8997a13d3c3bc2f25a24c0efe
n9wx <1602946346@qq.com>
2024-05-11 19:59:24 +0800

    [feat]add some constant

--- src/constants.rs
+++ src/constants.rs
@@ -6,3 +6,10 @@ pub const BOOT_STACK_SIZE: usize = PAGE_SIZE * 32;
 pub const KERNEL_HEAP_SIZE: usize = 0x100_0000;
 
 pub const MEMORY_END: usize = 0x8020_0000 + 0x100_0000;
+pub const CPU_NUMS: usize = 1;
+
+pub const GUEST_STACK_SIZE: usize = PAGE_SIZE * 16;
+
+pub const GUEST_STACK_BASE: usize = TRAMPOLINE - PAGE_SIZE;
+
+pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
