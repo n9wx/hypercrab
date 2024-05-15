af89f7b2d8caee4bb55060d67075d0f275002a34
n9wx <1602946346@qq.com>
2024-05-15 21:51:47 +0800

    [refactor]modify heap size and guest mem size

--- src/constants.rs
+++ src/constants.rs
@@ -3,13 +3,16 @@ pub const HYPERVISOR_EXTENSION: usize = 0x7;
 pub const PAGE_SIZE: usize = 0x1 << 12;
 pub const PAGE_SIZE_BITS: usize = 12;
 pub const BOOT_STACK_SIZE: usize = PAGE_SIZE * 32;
-pub const KERNEL_HEAP_SIZE: usize = 0x100_0000;
+pub const KERNEL_HEAP_SIZE: usize = 0x40_0000;
 
 pub const MEMORY_END: usize = 0x8020_0000 + 0x100_0000;
 pub const CPU_NUMS: usize = 1;
 
 pub const GUEST_STACK_SIZE: usize = PAGE_SIZE * 16;
 
-pub const GUEST_STACK_BASE: usize = TRAMPOLINE - PAGE_SIZE;
+pub const GUEST_STACK_TOP: usize = TRAMPOLINE - PAGE_SIZE;
+
+// 2 ^ 24 = 16M
+pub const GUEST_MEM_SIZE: usize = 0x100_0000;
 
 pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
