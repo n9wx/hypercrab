e3830196d135bf5e20f10f524f51d6763f3f43e2
n9wx <1602946346@qq.com>
2024-05-15 21:49:48 +0800

    [refactor]refactor TrapContext field type and flush hgatp when return to VS mode

--- src/arch/riscv/context.rs
+++ src/arch/riscv/context.rs
@@ -4,19 +4,20 @@ use riscv::register::{
     sstatus::{self, Sstatus, SPP},
 };
 
+// register type used in riscv::register is not ffi safe,so we just use usize
 #[repr(C)]
 #[derive(Copy, Clone)]
 pub struct TrapContext {
     // general purpose regs
     pub regs: [usize; 32],
-    // vsstatus for the hart
-    pub sstatus: Sstatus,
+    // sstatus for the hart
+    pub sstatus: usize,
     // trap pc in guest address space
     pub sepc: usize,
     // hstatus for vcpu context
-    pub hstatus: Hstatus,
+    pub hstatus: usize,
     // hgatp for vcpu context
-    pub hgatp: Hgatp,
+    pub hgatp: usize,
     // stack ptr in hypervisor address space
     pub guest_hyp_stack: usize,
     // address of trap_handler
@@ -28,22 +29,24 @@ impl TrapContext {
     pub fn set_sp(&mut self, sp: usize) {
         self.regs[2] = sp;
     }
+
+    /// set init context, include stack in hyp address space hgatp for the vcpu,return pl and address
+    /// in guest address space
     pub fn init_context(entry: usize, stack_ptr: usize, hgatp: usize, trap_handler: usize) -> Self {
         let mut sstatus = sstatus::read();
+        // return to s mode
         sstatus.set_spp(SPP::Supervisor);
         let mut hstatus = hstatus::read();
-        unsafe {
-            // return to vs mode
-            hstatus.set_spv(true);
-        }
+        // return to virtual pl(VS or VU)
+        hstatus.set_spv(true);
         Self {
             regs: [0; 32],
-            sstatus,
-            hstatus,
+            sstatus: sstatus.bits(),
+            hstatus: hstatus.bits(),
             sepc: entry,
             guest_hyp_stack: stack_ptr,
             trap_handler,
-            hgatp: Hgatp::from_bits(hgatp),
+            hgatp,
         }
     }
 }
